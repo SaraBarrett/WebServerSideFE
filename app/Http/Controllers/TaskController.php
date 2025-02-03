ae88e59d7679d0a5cc1bf6d7fe451875c18152ab
SaraBarrett <sara.c.mon@gmail.com>
2025-02-03 15:06:55 +0000

    taskss

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     public function returnAllTasksView(){
-        return view('tasks.all_tasks');
+        $tasksFromDB = $this->getAllTasks();
+
+        return view('tasks.all_tasks', compact('tasksFromDB'));
+    }
+
+    private function getAllTasks(){
+        $tasks = DB::table('tasks')
+        ->join('users', 'users.id','=', 'tasks.user_id')
+        ->select('tasks.*', 'users.name as user_name')
+        ->get();
+
+        return $tasks;
     }
 }
--- resources/views/tasks/all_tasks.blade.php
+++ resources/views/tasks/all_tasks.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.fe_layout')
 @section('content')
     <h3>Tasks vindas da BD</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Data Conclusão</th>
+                <th scope="col">User</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tasksFromDB as $task)
+                <tr>
+                    <th scope="row">{{ $task->id }}</th>
+                    <td>{{ $task->name }}</td>
+                    <td>{{ $task->status }}</td>
+                    <td>{{ $task->due_at }}</td>
+                    <td>{{ $task->user_name }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
